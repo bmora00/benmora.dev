4917ffa2e3913b55ccef0315bb2e07b9bdf7d097
bmora00 <bmora00@hotmail.com>
2026-01-05 18:12:11 -0600

    Last push before going to coffee shop

--- main.php
+++ main.php
@@ -194,7 +194,7 @@ $description = 'Everything you need to know about the contempory software develo
         </div>
         <div class="navbar-container">
             <nav class="navbar-menu nav-items">
-                <a class="navbar-menu-link" href="<?= $homeUrl ?>">Home</a>
+                <a class="active navbar-menu-link" href="<?= $homeUrl ?>">Home</a>
                 <!-- <a class="navbar-menu-link" href="/benistyping/projects.php">Projects</a> -->
                 <a class="navbar-menu-link" href="/benistyping/resume.php">More_Stuff</a>
             </nav>
@@ -232,11 +232,15 @@ $description = 'Everything you need to know about the contempory software develo
                         </svg>
                     </span>
                 </a>
-                <a class="social-icon navbar-icon" href="https://linkedin.com/in/bmora00" target="_blank" aria-label="linkedin">
+                <a class="social-icon navbar-icon" href="/benmoradev/src/documents/Ben_s_Resume_v2.pdf" target="_blank" aria-label="Resume Download">
                     <span class="social-container">
-                        <svg role="img" aria-label="LinkedIn social icon" class="social-svg" viewBox="0 0 65 78">
+                        <svg role="img" aria-label="Dounload icon" class="social-svg" viewBox="0 0 65 78">
                             <g class="social-svg-icon">
-                                <path class="svg-icon" d='M53.7692 27.2093H46.4729V4.53488C46.4729 2.0407 44.4079 0 41.884 0H23.5286C21.0047 0 18.9397 2.0407 18.9397 4.53488V27.2093H11.6434C7.55931 27.2093 5.49432 32.107 8.3853 34.9639L29.4482 55.7791C31.2379 57.5477 34.1289 57.5477 35.9185 55.7791L56.9814 34.9639C59.8724 32.107 57.8533 27.2093 53.7692 27.2093ZM0.584229 72.5581C0.584229 75.0523 2.64922 77.093 5.1731 77.093H60.2395C62.7634 77.093 64.8284 75.0523 64.8284 72.5581C64.8284 70.0639 62.7634 68.0232 60.2395 68.0232H5.1731C2.64922 68.0232 0.584229 70.0639 0.584229 72.5581Z'
+                                <path class="svg-icon" d="M53.7692 27.2093H46.4729V4.53488C46.4729 2.0407 44.4079 0 41.884 0
+                                    H23.5286C21.0047 0 18.9397 2.0407 18.9397 4.53488V27.2093H11.6434C7.55931 27.2093 5.49432 32.107 8.3853 34.9639L29.4482 55.7791
+                                    C31.2379 57.5477 34.1289 57.5477 35.9185 55.7791L56.9814 34.9639C59.8724 32.107 57.8533 27.2093 53.7692 27.2093ZM0.584229 72.5581
+                                    C0.584229 75.0523 2.64922 77.093 5.1731 77.093H60.2395C62.7634 77.093 64.8284 75.0523 64.8284 72.5581
+                                    C64.8284 70.0639 62.7634 68.0232 60.2395 68.0232H5.1731C2.64922 68.0232 0.584229 70.0639 0.584229 72.5581Z"
                                 ></path>
                             </g>
                         </svg>
@@ -253,14 +257,14 @@ $description = 'Everything you need to know about the contempory software develo
                 <img class="landing-svg" src="/benmoradev/ben-sig.svg" alt="benmora.dev logo">
             </div>
             <div class="landing-content">
-                <div class="landing-title">
-                    <!-- <span id=name>Ben Mora</span> -->
+                <!-- <div class="landing-title">
+                    <span id=name>Ben Mora</span>
                     <span id=tagline>
                         Something <br/>
                         Something <br/>
                         Something 
                     </span>
-                </div>
+                </div> -->
                 <div class="landing-text">
                     <p>
                         This site is where I collect all the hobby projects I have completed. It is very much a continuous work in progress. I love 
@@ -278,6 +282,29 @@ $description = 'Everything you need to know about the contempory software develo
         <img class="landing-image" src="/benmoradev/mountain-landing.png" alt="Mountain Landscape">
     </div> -->
     <div class="main-wrapper">
+        <svg xmlns="http://www.w3.org/2000/svg" id="a" viewBox="0 0 600 1500">
+            <defs><style>.b{fill:none;stroke:#fff;stroke-linecap:round;stroke-linejoin:round;stroke-width:8px;}</style></defs>
+            <g>
+                <path class="b" d="M122.73-69.66V524.66c0,34.57,28.03,62.6,62.6,62.6h118.4c6.08,0,11,4.92,11,11v922.94"/>
+                <path class="b" d="M131.51-78.51V523.31c0,30.43,24.66,55.09,55.09,55.09h118.6c10.11,0,18.31,8.2,18.31,18.31v934.88"/>
+                <path class="b" d="M140.29-87.36V521.97c0,26.28,21.3,47.58,47.58,47.58h118.79c14.15,0,25.63,11.47,25.63,25.63v946.81"/>
+                <path class="b" d="M149.07-96.22V520.63c0,22.13,17.94,40.07,40.07,40.07h118.99c18.19,0,32.94,14.75,32.94,32.94v958.75"/>
+                <path class="b" d="M157.85-105.07V519.29c0,17.98,14.58,32.56,32.56,32.56h119.18c22.23,0,40.26,18.02,40.26,40.26v970.68"/>
+                <path class="b" d="M166.63-113.92V517.94c0,13.83,11.21,25.05,25.05,25.05h119.38c26.27,0,47.57,21.3,47.57,47.57v982.62"/>
+                <path class="b" d="M175.41-122.78V516.6c0,9.69,7.85,17.54,17.54,17.54h119.58c30.31,0,54.89,24.57,54.89,54.89v994.55"/>
+                <path class="b" d="M184.18-131.63V515.26c0,5.54,4.49,10.03,10.03,10.03h119.77c34.35,0,62.2,27.85,62.2,62.2v1006.49"/>
+            </g>
+            <g>
+                <path class="b" d="M218.73,1699.49v-594.31c0-34.57,28.03-62.6,62.6-62.6h118.4c6.08,0,11-4.92,11-11V-15.74"/>
+                <path class="b" d="M227.51,1708.34v-601.82c0-30.43,24.66-55.09,55.09-55.09h118.6c10.11,0,18.31-8.2,18.31-18.31V-16.12"/>
+                <path class="b" d="M236.29,1717.2v-609.33c0-26.28,21.3-47.58,47.58-47.58h118.79c14.15,0,25.63-11.47,25.63-25.63V-16.49"/>
+                <path class="b" d="M245.07,1726.05v-616.84c0-22.13,17.94-40.07,40.07-40.07h118.99c18.19,0,32.94-14.75,32.94-32.94V-16.87"/>
+                <path class="b" d="M253.85,1734.9v-624.36c0-17.98,14.58-32.56,32.56-32.56h119.18c22.23,0,40.26-18.02,40.26-40.26V-17.24"/>
+                <path class="b" d="M262.63,1743.76v-631.87c0-13.83,11.22-25.05,25.05-25.05h119.38c26.27,0,47.57-21.3,47.57-47.57V-17.62"/>
+                <path class="b" d="M271.4,1752.61v-639.38c0-9.69,7.85-17.54,17.54-17.54h119.58c30.31,0,54.89-24.57,54.89-54.89V-18"/>
+                <path class="b" d="M280.18,1761.46v-646.89c0-5.54,4.49-10.03,10.03-10.03h119.77c34.35,0,62.2-27.85,62.2-62.2V-18.37"/>
+            </g>
+        </svg>
         <main class="main-container">
             <div class="about-container main-content">
                 <div>
@@ -321,10 +348,12 @@ $description = 'Everything you need to know about the contempory software develo
                 </div>
             </div> -->
             <div class="contact-wrapper">
-                <div class="contact-container">
-                    <div class="contact-content main-content">
-                        <h2>Contact Me</h2>
-                        <p>If you'd like to get in touch, feel free to reach out via email at <a href="mailto:bmora00@hotmail.com"></a>
+                <div class="contact-container main-content">
+                    <div class="contact-content">
+                        <h2>Contact</h2>
+                        <p>Please reach out to me. I want to talk to you</p>
+                        <p class="contact-link"><a href="mailto:bmora00@hotmail.com">bmora00@hotmail.com</a></p>
+                        <p class="contact-link">LinkedIn: @bmora00</p>
                     </div>
                 </div>
             </div>
